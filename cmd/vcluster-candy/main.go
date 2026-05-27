9b91db90bb4d8a11ecc34c6f4ec7efaf8bfd4916
Catalin Balan <catalin.balan@loft.sh>
2026-05-27 23:51:19 +0200

    Cache only pods on the same node

--- cmd/vcluster-candy/main.go
+++ cmd/vcluster-candy/main.go
@@ -10,10 +10,13 @@ import (
 	"github.com/loft-sh/vcluster-candy/pkg/util"
 	"github.com/miekg/dns"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -35,17 +38,33 @@ func main() {
 	var probeAddr string
 	var internalDomainsString string
 	var resolvconf string
+	var nodeName string
 	flag.StringVar(&dnsAddr, "dns-bind-address", ":53", "The address the dns server binds to.")
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":9153", "The address the metrics endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.StringVar(&internalDomainsString, "internal-domain", "cluster.local", "The internal domains to use for the DNS server.")
 	flag.StringVar(&resolvconf, "resolvconf", "/etc/resolv.conf", "The resolv.conf file to use for the DNS server.")
+	flag.StringVar(&nodeName, "node-name", "", "The node name to filter pods by.")
 
 	// logger setup
 	opts := zap.Options{Development: true}
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	// nodeName flag validation
+	if nodeName == "" {
+		log.Error(nil, "--node-name flag is required")
+		os.Exit(1)
+	}
+
+	log.Info("starting vcluster-candy with options",
+		"nodeName", nodeName,
+		"dnsAddr", dnsAddr,
+		"metricsAddr", metricsAddr,
+		"probeAddr", probeAddr,
+		"internalDomains", internalDomainsString,
+		"resolvconf", resolvconf)
 
 	// new controller-runtime manager
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
@@ -54,6 +73,13 @@ func main() {
 			BindAddress: metricsAddr,
 		},
 		HealthProbeBindAddress: probeAddr,
+		Cache: cache.Options{
+			ByObject: map[client.Object]cache.ByObject{
+				&corev1.Pod{}: {
+					Field: fields.SelectorFromSet(fields.Set{"spec.nodeName": nodeName}),
+				},
+			},
+		},
 	})
 	if err != nil {
 		log.Error(err, "Failed to create manager")
